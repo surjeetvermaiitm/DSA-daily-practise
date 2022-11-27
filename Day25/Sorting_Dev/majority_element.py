16e79f307d235ea03e1bf9c24ef8c12f56703a28
surjeetvermaiitm <surjeetverma2017@gmail.com>
2022-11-27 15:00:25 +0530

    Day25

---
+++ Day25/Sorting_Dev/majority_element.py
@@ -0,0 +1,25 @@
+#Link: https://leetcode.com/problems/majority-element/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # n=len(nums)
+        # m={n:nums.count(n) for n in nums}
+        # print(m)
+        # for k,v in m.items():
+        #     if v>n/2:
+        #         return k
+        # nums.sort()
+        # ct=1
+        # n=len(nums)
+        # if n==1:
+        #     return nums[0]
+        # for i in range(len(nums)-1):
+        #     if nums[i]==nums[i+1]:
+        #         ct+=1
+        #         if ct>n/2:
+        #             return nums[i]
+        #     else:
+        #         ct=1
+        
+
+                
